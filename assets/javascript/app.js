a5fda738acdc963f502a0439892cf79206e4209c
Tristin Tobias <tristintobias@gmail.com>
2018-07-07 18:46:50 -0700

    added ratings and changes to styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // array of topics
-var qArray = ['dog', 'cat', 'bird', 'goldfish', 'harry potter', 'star wars', 'nature', 'waterfall', 'coders', 'college', 'paranormal', 'creepy'];    
+var qArray = ['dog', 'cat', 'bird', 'fish', 'Harry Potter', 'Star Wars', 'Lord of the Rings', 'nature', 'waterfall', 'landscape', 'scary', 'paranormal', 'creepy'];    
 
 // adds buttons to page using array of topics
 for (index in qArray){
@@ -9,17 +9,20 @@ for (index in qArray){
 
 // adds new topic buttons to page
 $(document).on('click', '#addButton', function(){
+    event.preventDefault();
     var userInput = $('input').val();
     if (userInput != ''){
         var button = $('<button>').addClass('btn-danger topic').text(userInput);
         $('#buttons').append(button);
     }
+    $('input').val('');
 });
 
 // calls the giphy API on click of topic buttons
 $(document).on('click', '.topic', function(){
     event.preventDefault();
     $('#gifDiv').empty();
+    $('#gifDiv').append('<h3>Ten Random Gifs</h3><h4>Click to animate/pause</h4>')
     var q = $(this).text().trim();
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aydEzGVPxXsttRYtmMJTesG1vc9bIeS2&q=" + q + "&limit=100&offset=0&lang=en";
     $.ajax({
@@ -31,8 +34,12 @@ $(document).on('click', '.topic', function(){
             var ranNum = Math.floor(Math.random()*100);
             var gifUrlStill = response.data[ranNum].images.original_still.url;
             var gifUrlActive = response.data[ranNum].images.original.url;
+            var rating = response.data[ranNum].rating;
             var gif = $('<img src=' + gifUrlStill + '>').addClass('giphys').attr('data-state', 'still').attr('data-still', gifUrlStill).attr('data-active', gifUrlActive);
-            $('#gifDiv').append(gif); 
+            $('#gifDiv').append(gif);
+            $('#gifDiv').append('<br>Rating: ');
+            $('#gifDiv').append(rating);
+            $('#gifDiv').append('<br><br>');
         }
         $('.giphys').on('click', function(){
             var state = $(this).attr('data-state');
